393e315936e97d7bd83fa34d70079238e7c7b270
Track-o <trackosys@gmail.com>
2024-09-11 22:17:45 +0200

    Update GoldNugget.py
    SilverNugget src

--- GoldNugget.py
+++ GoldNugget.py
@@ -1,37 +1,40 @@
-from os import path, mkdir, listdir, getcwd
-from cv2 import imread, GaussianBlur, imwrite
+import os
+import cv2
 
 print('\n')
-#dir_path = path.dirname(path.realpath(__file__))
-dir_path = getcwd()
-input_dir = path.join(dir_path,'input')
-output_dir = path.join(dir_path,'output')
-if path.exists(output_dir):
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = os.getcwd()
+input_dir = os.path.join(dir_path,'input')
+output_dir = os.path.join(dir_path,'output')
+if os.path.exists(output_dir):
     check_dir = True
 else :
     check_dir = False
 if check_dir == False :
-    mkdir(output_dir)
-if path.exists(input_dir):
+    os.mkdir(output_dir)
+if os.path.exists(input_dir):
     check_dir = True
 else :
     check_dir = False
 if check_dir == False :
-    mkdir(input_dir)
+    os.mkdir(input_dir)
 print(dir_path)
 print('\n',input_dir)
 print('\n',output_dir)
+
 print('\n','filename :','\n')
-for filename in listdir(input_dir):
+for filename in os.listdir(input_dir):
     print(filename,'\n')
+
 print('\n','process :','\n')
-for filename in listdir(input_dir):
-    f = path.join(input_dir,filename)
-    extension = path.splitext(f)
-    if path.isfile(f):
+for filename in os.listdir(input_dir):
+    f = os.path.join(input_dir,filename)
+    extension = os.path.splitext(f)
+    if os.path.isfile(f):
         if '.png' in extension[1]:
             print(f,'\n')
-            layer_X_X = imread(f)
-            layer_X_X_blur = GaussianBlur(layer_X_X,(301,301),0)
-            imwrite(path.join(output_dir,filename),layer_X_X_blur)
+            layer_X_X = cv2.imread(f)
+            layer_resize = cv2.resize(layer_X_X,(1024*8,1024*8),fx=1.0,fy=1.0)
+            layer_X_X_blur = cv2.GaussianBlur(layer_resize,(301,301),0)
+            cv2.imwrite(os.path.join(output_dir,filename),layer_X_X_blur)
 input("Press enter to exit;")
